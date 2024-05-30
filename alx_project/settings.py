57fe1efafae7e44bdddbd34ca52958c8732e5f96
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2024-05-30 09:17:43 +0100

    ADEED DOCKER

--- alx_project/settings.py
+++ alx_project/settings.py
@@ -9,9 +9,18 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,28 +34,49 @@ SECRET_KEY = "django-insecure-+v&jn-tu3uj2xpo0ne5v)-i4xo9444$+f_9+cv)fphxhtfos+e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+SHARED_APPS = [
+    'channels',
+    'django_tenants',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core'
 ]
 
+TENANT_APPS = [
+            #    'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.auth',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
+            'django.contrib.sessions',
+            'django.contrib.admin',
+            'users.apps.UsersConfig',
+            "base.apps.BaseConfig",
+        
+        ]
+
+INSTALLED_APPS = SHARED_APPS + [app for app in TENANT_APPS if app not in SHARED_APPS]
+
+
+
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django_tenants.middleware.main.TenantMainMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = "alx_project.urls"
@@ -54,7 +84,9 @@ ROOT_URLCONF = "alx_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [
+                BASE_DIR / "templates"
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -68,17 +100,43 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "alx_project.wsgi.application"
+ASGI_APPLICATION = 'alx_project.asgi.application'
 
+
+CHANNEL_LAYERS = {
+    'default': {
+    'BACKEND': 'channels_redis.core.RedisChannelLayer',
+    "BACKEND": "channels.layers.InMemoryChannelLayer",
+    # 'CONFIG': {
+    #     "hosts": [('127.0.0.1', 6379),],
+    #     },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django_tenants.postgresql_backend',
+        'NAME': 'blog_postgres',
+        'USER': 'blog_postgres',
+        'PASSWORD': 'blog_postgres',
+        'HOST': 'localhost',   # or postgres
+        'PORT': 5432
     }
 }
+
+DATABASE_ROUTERS = (
+    'django_tenants.routers.TenantSyncRouter',
+)
 
 
 # Password validation
@@ -99,6 +157,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -117,7 +176,19 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+TENANT_MODEL = "core.Client"
+
+TENANT_DOMAIN_MODEL = "core.Domain"
+
+PUBLIC_SCHEMA_URLCONF = "core.urls"
+PUBLIC_SCHEMA_NAME = 'public'
